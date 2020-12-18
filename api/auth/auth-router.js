9eadd1b9b60efc3e19592d1023034068e9282a0a
Andrew Poppenberg <andrewpoppenberg@yahoo.com>
2020-12-18 11:10:22 -0600

    hope test passes

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -24,7 +24,7 @@ const checker = async (req, res, next) => {
       res.status(400).json("username taken");
     } else {
       next();
-      }
+    }
   } catch (err) {
     res.status(404).json(err.message);
   }
